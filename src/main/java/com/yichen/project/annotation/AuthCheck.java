4d0184585c53868956caba1503260b0e6013c6ab
mojiez <937691466@qq.com>
2024-05-24 18:48:16 +0800

    使用springboot init完成项目搭建，修改了数据库和包目录

---
+++ src/main/java/com/yichen/project/annotation/AuthCheck.java
@@ -0,0 +1,26 @@
+package com.yichen.project.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 权限校验
+ * 自定义的权限校验的注解（只允许某个角色访问某个接口） 注解的知识
+ * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
+ * @from <a href="https://yupi.icu">编程导航知识星球</a>
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface AuthCheck {
+
+    /**
+     * 必须有某个角色
+     *
+     * @return
+     */
+    String mustRole() default "";
+
+}
+
